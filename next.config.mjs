26bd09b143bdc2d9c69a9d983397b6854a676703
root <root@Efe-Pc>
2024-02-12 22:00:16 +0300

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: '/spine-website',
-  assetPrefix: '/spine-website/',
+  basePath: "/spine-website",
+  assetPrefix: "/spine-website/",
+  output: "export",
 };
 
 export default nextConfig;
