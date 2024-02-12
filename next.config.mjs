371f7cdd3026e09164bc2d407c8313be3748c977
root <root@Efe-Pc>
2024-02-12 22:03:28 +0300

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,10 @@ const nextConfig = {
   basePath: "/spine-website",
   assetPrefix: "/spine-website/",
   output: "export",
+  images: {
+    loader: "akamai",
+    path: "",
+  },
 };
 
 export default nextConfig;
