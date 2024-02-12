a95667a0a8592170db8b3a3ab7077e2a65df57bf
root <root@Efe-Pc>
2024-02-12 22:09:22 +0300

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: "/spine-website",
-  assetPrefix: "/spine-website/",
-  output: "export",
-  images: {
-    loader: "akamai",
-    path: "",
-  },
 };
 
 export default nextConfig;
