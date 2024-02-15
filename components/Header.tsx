6adbffe9b40662c87b345e35935ebe24950d2d5b
root <root@Efe-Pc>
2024-02-15 16:05:59 +0300

    member area photos circle, added 2 logos below hero title

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,71 +6,67 @@ export default function Header() {
   const [activeNavItem, setActiveNavItem] = useState("#home");
 
   return (
+    <div className="navbar-area">
+      <div className="container">
+        <div className="row align-items-center">
+          <div className="col-lg-12">
+            <nav className="navbar navbar-expand-lg">
+              <button
+                className="navbar-toggler"
+                type="button"
+                onClick={() => setIsNavExpanded(!isNavExpanded)}
+                aria-controls="navbarSupportedContent"
+                aria-expanded={isNavExpanded ? "true" : "false"}
+                aria-label="Toggle navigation"
+              >
+                <span className="toggler-icon"></span>
+                <span className="toggler-icon"></span>
+                <span className="toggler-icon"></span>
+              </button>
 
-      <div className="navbar-area">
-        <div className="container">
-          <div className="row align-items-center">
-            <div className="col-lg-12">
-              <nav className="navbar navbar-expand-lg">
-              <a className="navbar-brand" href="index.html">
-                  <Image src={"/Logo.png"} layout="fill" alt={"logo"} />
-                </a>
-                <button
-                  className="navbar-toggler"
-                  type="button"
-                  onClick={() => setIsNavExpanded(!isNavExpanded)}
-                  aria-controls="navbarSupportedContent"
-                  aria-expanded={isNavExpanded ? "true" : "false"}
-                  aria-label="Toggle navigation"
-                >
-                  <span className="toggler-icon"></span>
-                  <span className="toggler-icon"></span>
-                  <span className="toggler-icon"></span>
-                </button>
-
-                <div
-                  className={`collapse navbar-collapse sub-menu-bar ${
-                    isNavExpanded ? "show" : ""
-                  }`}
-                  id="navbarSupportedContent"
-                >
-                  <ul id="nav" className="navbar-nav ms-auto">
-                    <li className="nav-item">
-                      <a className="page-scroll" href="#home">
-                        Anasayfa
-                      </a>
-                    </li>
-                    <li className="nav-item">
-                      <a className="page-scroll" href="#program">
-                        Program
-                      </a>
-                    </li>
-                    <li className="nav-item">
-                      <a className="page-scroll" href="#meet-the-team">
-                        Düzenleme Kurulu
-                      </a>
-                    </li>
-                    <li className="nav-item">
-                      <a className="page-scroll" href="#announcement">
-                        Duyuru
-                      </a>
-                    </li>
-                    <li className="nav-item">
-                      <a className="" href="#faculty">
-                        Fakülte
-                      </a>
-                    </li>
-                    <li className="nav-item">
-                      <a className="" href="#contact">
-                        İletişim
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-              </nav>
-            </div>
+              <div
+                className={`collapse navbar-collapse sub-menu-bar ${
+                  isNavExpanded ? "show" : ""
+                }`}
+                id="navbarSupportedContent"
+              >
+                <ul id="nav" className="navbar-nav ms-auto">
+                  <li className="nav-item">
+                    <a className="page-scroll" href="#home">
+                      Anasayfa
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="page-scroll" href="#program">
+                      Program
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="page-scroll" href="#meet-the-team">
+                      Düzenleme Kurulu
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="page-scroll" href="#announcement">
+                      Duyuru
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="" href="#faculty">
+                      Fakülte
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="" href="#contact">
+                      İletişim
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </nav>
           </div>
         </div>
       </div>
+    </div>
   );
 }
--- components/HeroArea.tsx
+++ components/HeroArea.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 export default function HeroArea() {
@@ -9,9 +10,25 @@ export default function HeroArea() {
             <div className="col-lg-6">
               <div className="hero-content">
                 <h1>Omurga Cerrahisinde Komplikasyonlar</h1>
-                <p style={{fontSize: 32}}>Tümör, Dejeneratif, Deformite</p>
+                <p style={{ fontSize: 32 }}>Tümör, Dejeneratif, Deformite</p>
                 <h3>16 Mart 2024</h3>
                 <h4> Medicana Ataköy Hastanesi, İstanbul</h4>
+              </div>
+              <div className="row">
+                <div className="col-3">
+                  <a className="navbar-brand" href="index.html">
+                    <Image src={"/arel-logo.png"} layout="fill" alt={"logo"} />
+                  </a>
+                </div>
+                <div className="col-3">
+                  <a className="navbar-brand" href="index.html">
+                    <Image
+                      src={"/medicana-logo.png"}
+                      layout="fill"
+                      alt={"logo"}
+                    />
+                  </a>
+                </div>
               </div>
             </div>
           </div>
